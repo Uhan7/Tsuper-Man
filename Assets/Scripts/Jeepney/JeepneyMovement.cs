cca3d2916c81362ed54fb498b26fa8cfffb1a13c
Uhan7 <luckyfellowfella@gmail.com>
2025-06-02 22:46:15 +0800

    atr fugg it thsi is good enough for me
    things to do:
    - make cinemachine effects when faster
    - make moving forward after breaking faster
    - maybe checkr otation and how it feels again toms
    - try setting up pseudo map

--- Assets/Scripts/Jeepney/JeepneyMovement.cs
+++ Assets/Scripts/Jeepney/JeepneyMovement.cs
@@ -5,6 +5,9 @@ public class JeepneyMovement : MonoBehaviour
     // Components
     private Rigidbody2D rb;
 
+    // Checks
+    [SerializeField] private float minimumSpeedForTurn;
+
     // Values
     [SerializeField] private float normalSpeed;
     [SerializeField] private float normalSpeedMax;
@@ -19,9 +22,15 @@ public class JeepneyMovement : MonoBehaviour
 
     [SerializeField] private float friction;
 
+    private float directionFactor;
+    [SerializeField] private float turningDecay;
+
     // Inputs
     [SerializeField] private KeyCode accelerateKey;
     [SerializeField] private KeyCode decelerateKey;
+    [SerializeField] private KeyCode turnRightKey;
+    [SerializeField] private KeyCode turnLeftKey;
+
     [SerializeField] private KeyCode boostKey;
 
     [SerializeField] private KeyCode debugKey;
@@ -29,6 +38,9 @@ public class JeepneyMovement : MonoBehaviour
     // Flags
     private bool accelerating;
     private bool decelerating;
+    private bool turningRight;
+    private bool turningLeft;
+
     private bool boosting;
 
     private bool canGoForward;
@@ -50,7 +62,7 @@ public class JeepneyMovement : MonoBehaviour
     {
         InputCheck();
 
-        if (Input.GetKeyDown(debugKey))PrintDebugs();
+        if (Input.GetKeyDown(debugKey)) PrintDebugs();
     }
 
     private void FixedUpdate()
@@ -58,6 +70,12 @@ public class JeepneyMovement : MonoBehaviour
         if (decelerating) Decelerate();
         else if (accelerating) Accelerate();
         else ApplyFriction();
+
+        if (turningRight) Turn('R');
+        else if (turningLeft) Turn('L');
+        else directionFactor = 0;
+
+        ApplyTurningFriction();
         LimitSpeed();
     }
 
@@ -68,6 +86,9 @@ public class JeepneyMovement : MonoBehaviour
         accelerating = Input.GetKey(accelerateKey);
         decelerating = Input.GetKey(decelerateKey);
         boosting = Input.GetKey(boostKey);
+
+        turningRight = Input.GetKey(turnRightKey);
+        turningLeft = Input.GetKey(turnLeftKey);
     }
 
     // FixedUpdate Functions ---------------------------------------------------
@@ -76,18 +97,48 @@ public class JeepneyMovement : MonoBehaviour
     {
         float speed = normalSpeed; // Temporarily
 
-        if (canGoForward) rb.AddForce(transform.up * normalSpeed, ForceMode2D.Force);
+        if (canGoForward) rb.AddForce(rb.transform.up * normalSpeed, ForceMode2D.Force);
     }
 
     void Decelerate()
     {
         ApplyFriction();
-        rb.AddForce(-transform.up * deceleratingSpeed, ForceMode2D.Force);
+        rb.AddForce(-rb.transform.up * deceleratingSpeed, ForceMode2D.Force);
+    }
+
+    void Turn(char direction)
+    {
+        float turnSpeed = normalTurningSpeed; // Temporarily
+        float maxSpeed = normalSpeedMax; // Temporarily
+
+        if (direction == 'R' && directionFactor > 0 || direction == 'L' && directionFactor < 0) directionFactor = 0;
+
+        if (direction == 'R') directionFactor -= turningDecay;
+        else if (direction == 'L') directionFactor += turningDecay;
+
+        if (decelerating && directionFactor > 0) directionFactor = -1;
+        else if (decelerating && directionFactor < 0) directionFactor = 1;
+
+        directionFactor = Mathf.Clamp(directionFactor, -1, 1);
+
+        // Guard Clause, so we can retain the wheel turn but not actually turn it
+        if (rb.linearVelocity.magnitude < minimumSpeedForTurn) return;
+
+        float turnScale = Mathf.Clamp(rb.linearVelocity.magnitude * 1.5f, -deceleratingSpeedMax, deceleratingSpeedMax) / deceleratingSpeedMax;
+
+        rb.angularVelocity = (30f + turnSpeed * turnScale) * directionFactor;
     }
 
     void ApplyFriction()
     {
         rb.linearVelocity *= friction;
+    }
+
+    void ApplyTurningFriction()
+    {
+        Vector2 forward = rb.transform.up;
+        float forwardSpeed = Vector2.Dot(rb.linearVelocity, forward);
+        rb.linearVelocity = forward * forwardSpeed;
     }
 
     void LimitSpeed()
@@ -105,6 +156,6 @@ public class JeepneyMovement : MonoBehaviour
 
     void PrintDebugs()
     {
-        Debug.Log(rb.transform.up);
+        Debug.Log(rb.angularVelocity);
     }
 }
