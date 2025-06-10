b712c876da3548ae38fbf58216b68e7cfe357cb9
Uhan7 <luckyfellowfella@gmail.com>
2025-06-11 01:06:03 +0800

    blockers + reacting drop locations

--- Assets/Scripts/People/DropLocation.cs
+++ Assets/Scripts/People/DropLocation.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class DropLocation : MonoBehaviour
 {
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public int ID;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Jeepney")
+        {
+            anim.Play("fade_in");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Jeepney")
+        {
+            anim.Play("fade_out");
+        }
+    }
 }
